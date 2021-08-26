ed368769c56706c9433e15f9181dbf8ed3cbc693
kentsheltongardner <kentsheltongardner@gmail.com>
2021-08-25 20:29:28 -0600

    Create styles.cs

---
+++ css/styles.cs
@@ -0,0 +1,108 @@
+html {
+    margin: 0px;
+    height: 100%;
+    width: 100%;
+}
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    color: rgba(64, 45, 21);
+    background-image: linear-gradient(#ffffff, #ecbd87);
+    background-image: url("../images/background.png");
+    background-repeat: repeat;
+    margin: 0px;
+    height: 100%;
+    width: 100%;
+}
+table {
+    border-style: solid;
+    border-width: 3px;
+    border-color: rgba(64, 45, 21);
+    background-color: rgba(0, 0, 0, 0.15);
+    table-layout: fixed;
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+}
+h1 {
+    font-size: 50px;
+}
+th {
+    text-align: left;
+    vertical-align: top;
+    border-style: solid;
+    border-width: 1px;
+    border-color: rgba(64, 45, 21);
+    border-spacing: 3px;
+    padding: 5px;
+}
+td {
+    text-align: left;
+    vertical-align: top;
+    border-style: solid;
+    border-width: 1px;
+    border-color: rgba(64, 45, 21);
+    border-spacing: 3px;
+    padding: 5px;
+}
+footer {
+    padding-top: 20px;
+    text-align: center;
+    font-size: 14px;
+}
+a {
+    color: rgba(129, 90, 51);
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: underline;
+}
+.section {
+    padding-left: 20px;
+}
+.engClass {
+    background-color: rgb(90, 152, 233, 0.3);
+}
+.commClass {
+    background-color: rgb(74, 192, 74, 0.3);
+}
+.pyClass {
+    background-color: rgb(218, 215, 77, 0.3);
+}
+.webClass {
+    background-color: rgb(219, 72, 72, 0.3);
+}
+.draftClass {
+    background-color: rgb(211, 74, 177, 0.3);
+}
+.portrait {
+    float: right;
+    text-align: center;
+}
+.portraitImg {
+    border-style: solid;
+    border-color: rgba(64, 45, 21);
+    border-width: 3;
+    width: 500px;
+}
+.poem {
+    padding-left: 20px;
+    font-style: italic;
+}
+.overlay {
+    position: absolute;
+    width: vw;
+    height: vh;
+    background: linear-gradient(rgba(129, 90, 51, 0.1), rgba(129, 90, 51, 0.5));
+}
+.content {
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+}
+.banner {
+    position: absolute;
+    width: 100%;
+    height: 120px;
+    z-index: -1;
+    background: linear-gradient(to right, rgba(129, 90, 51, 0.0), rgba(129, 90, 51, 0.5));
+}
